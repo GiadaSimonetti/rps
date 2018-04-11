d23a58bf499daee1c972c8667ebc3f685d1ab2e1
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-04-11 17:02:07 +0100

    Add jquery script and page

---
+++ src/interface.js
@@ -0,0 +1,27 @@
+alert("Hey");
+
+$(document).ready(function() {
+  //   playerChoice();
+  //   aiChoice();
+  //
+  //   function playerChoice(playerMove) {
+  //     if (playerMove === "rock") {
+  //       $("#player-choice").html("<i class='fa fa-hand-rock-o' aria-hidden="true"></i>");
+  //     } else if (playerMove === "paper"){
+  //       $("#player-choice").html("<i class='fa fa-hand-paper-o' aria-hidden="true"></i>");
+  //     } else {
+  //       $("#player-choice").html("<i class='fa fa-hand-scissors-o' aria-hidden="true"></i>");
+  //     };
+  //   };
+  //
+  //   function aiChoice(aiMove) {
+  //     if (aiMove === "rock") {
+  //       $("#ai-choice").html("<i class='fa fa-hand-rock-o' aria-hidden="true"></i>");
+  //     } else if (aiMove === "paper"){
+  //       $("#ai-choice").html("<i class='fa fa-hand-paper-o' aria-hidden="true"></i>");
+  //     } else {
+  //       $("#ai-choice").html("<i class='fa fa-hand-scissors-o' aria-hidden="true"></i>");
+  //     };
+  //   };
+  //
+});
