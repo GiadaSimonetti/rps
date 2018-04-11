3be4907542c1bc2e2ca7855f3232c1365c106bf8
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-04-11 16:06:25 +0100

    draw

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -11,47 +11,55 @@ describe("Feature test", function() {
     expect(player.currentMove).toEqual("paper");
   });
 
-  it("returns 'It's a draw!' when 'rock'-'rock'", function() {
-    expect(game.match("rock", "rock")).toEqual("It's a draw!");
+  it("returns 'It's a draw!' when player1 is equal player2 ('rock' - 'rock')", function() {
+    expect(game.matchDraw("rock", "rock")).toEqual("It's a draw!");
   });
 
-  it("returns 'paper wins' when 'rock'-'paper'", function() {
-    expect(game.match("rock", "paper")).toEqual("paper wins!");
+  it("returns 'It's a draw!' when  player1 is equal player2 ('paper'-'paper')", function() {
+    expect(game.matchDraw("paper", "paper")).toEqual("It's a draw!");
   });
 
-  it("returns 'rock wins' when 'rock'-'scissors'", function() {
-    expect(game.match("rock", "scissors")).toEqual("rock wins!");
+  it("returns 'It's a draw!' when player1 is equal player2 ('scissors'-'scissors')", function() {
+    expect(game.matchDraw("scissors", "scissors")).toEqual("It's a draw!");
   });
 
-  it("returns 'rock wins' when 'rock'-'lizard'", function() {
-    expect(game.match("rock", "scissors")).toEqual("rock wins!");
+  it("returns 'It's a draw!' when player1 is equal player2 ('lizard'-'lizard')", function() {
+    expect(game.matchDraw("lizard", "lizard")).toEqual("It's a draw!");
   });
 
-  it("returns 'Spock wins' when 'rock'-'Spock'", function() {
-    expect(game.match("rock", "Spock")).toEqual("Spock wins!");
+  it("returns 'It's a draw!' when player1 is equal player2 ('Spock'-'Spock')", function() {
+    expect(game.matchDraw("Spock", "Spock")).toEqual("It's a draw!");
   });
 
-  it("returns 'paper wins' when 'paper'-'rock'", function() {
-    expect(game.match("paper", "rock")).toEqual("paper wins!");
-  });
-
-  it("returns 'It's a draw!' when 'paper'-'paper'", function() {
-    expect(game.match("paper", "paper")).toEqual("It's a draw!");
-  });
-
-  it("returns 'scissors wins' when 'paper'-'scissors'", function() {
-    expect(game.match("paper", "scissors")).toEqual("scissors wins!");
-  });
-
-  it("returns 'rock wins' when 'scissors'-'rock'", function() {
-    expect(game.match("scissors", "rock")).toEqual("rock wins!");
-  });
-
-  it("returns 'scissors wins' when 'scissors'-'paper'", function() {
-    expect(game.match("scissors", "paper")).toEqual("scissors wins!");
-  });
-
-  it("returns 'It's a draw!' when 'scissors'-'scissors'", function() {
-    expect(game.match("scissors", "scissors")).toEqual("It's a draw!");
-  });
+  // it("returns 'paper wins' when 'rock'-'paper'", function() {
+  //   expect(game.match("rock", "paper")).toEqual("paper wins!");
+  // });
+  //
+  // it("returns 'rock wins' when 'rock'-'scissors'", function() {
+  //   expect(game.match("rock", "scissors")).toEqual("rock wins!");
+  // });
+  //
+  // it("returns 'rock wins' when 'rock'-'lizard'", function() {
+  //   expect(game.match("rock", "scissors")).toEqual("rock wins!");
+  // });
+  //
+  // it("returns 'Spock wins' when 'rock'-'Spock'", function() {
+  //   expect(game.match("rock", "Spock")).toEqual("Spock wins!");
+  // });
+  //
+  // it("returns 'paper wins' when 'paper'-'rock'", function() {
+  //   expect(game.match("paper", "rock")).toEqual("paper wins!");
+  // });
+  //
+  // it("returns 'scissors wins' when 'paper'-'scissors'", function() {
+  //   expect(game.match("paper", "scissors")).toEqual("scissors wins!");
+  // });
+  //
+  // it("returns 'rock wins' when 'scissors'-'rock'", function() {
+  //   expect(game.match("scissors", "rock")).toEqual("rock wins!");
+  // });
+  //
+  // it("returns 'scissors wins' when 'scissors'-'paper'", function() {
+  //   expect(game.match("scissors", "paper")).toEqual("scissors wins!");
+  // });
 });
--- src/game.js
+++ src/game.js
@@ -1,29 +1,7 @@
 function Game(player) {}
 
-Game.prototype.match = function(player1, player2) {
-  if (player1 === "rock") {
-    if (player2 === "rock") {
-      return "It's a draw!";
-    } else if (player2 === "paper") {
-      return player2 + " wins!";
-    } else if (player2 === "lizard") {
-      return player1 + " wins!";
-    }
-  } else if (player1 === "paper") {
-    if (player2 === "rock") {
-      return player1 + " wins!";
-    } else if (player2 === "paper") {
-      return "It's a draw!";
-    } else if (player2 === "scissors") {
-      return player2 + " wins!";
-    }
-  } else if (player1 === "scissors") {
-    if (player2 === "rock") {
-      return player2 + " wins!";
-    } else if (player2 === "paper") {
-      return player1 + " wins!";
-    } else if (player2 === "scissors") {
-      return "It's a draw!";
-    }
+Game.prototype.matchDraw = function(player1, player2) {
+  if (player1 === player2) {
+    return "It's a draw!";
   }
 };
