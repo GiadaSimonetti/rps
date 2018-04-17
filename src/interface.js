0a79a191cf15ff33827c9086244630f3fdd30bf2
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-04-17 11:52:44 +0100

    refactoring

--- src/interface.js
+++ src/interface.js
@@ -1,9 +1,9 @@
 alert("Hey");
 
 $(document).ready(function() {
-  var player1 = new Player
-  var player2 = new Player1
-  var game = new Game
+  var player1 = new Player();
+  var player2 = new Player();
+  var game = new Game();
 
   $("#btnMovePlayer1").click(function() {
   event.preventDefault();
@@ -11,14 +11,14 @@ $(document).ready(function() {
   displayMovePlayer1(movePlayer1);
 });
 
-    function displayMovePlayer1(move) {
-      if (move === "rock") {
+    function displayMovePlayer1(movePlayer1) {
+      if (movePlayer1 === "rock") {
         $("#player1").html("<i class='fa fa-hand-rock-o' aria-hidden="true"></i>");
-      } else if (move === "paper"){
+      } else if (movePlayer1 === "paper"){
         $("#player1").html("<i class='fa fa-hand-paper-o' aria-hidden="true"></i>");
-      } else if (move === "scissors"){
-        $("#player1").html("<i class='fa fa-hand-scissors-o' aria-hidden="true"></i>")
-      } else if (move === "lizard"){
+      } else if (movePlayer1 === "scissors"){
+        $("#player1").html("<i class='fa fa-hand-scissors-o' aria-hidden="true"></i>");
+      } else if (movePlayer1 === "lizard"){
         $("#player1").html("<i class='fa-hand-lizard-o' aria-hidden="true"></i>");
       } else {
         $("#player1").html("<i class='fa-hand-spock-o' aria-hidden="true"></i>");
