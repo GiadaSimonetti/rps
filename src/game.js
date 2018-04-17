e4f41d0753c9bc93ed92368fff9d3266a25be5c4
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-04-17 14:12:43 +0100

    Add result function in Game

--- src/game.js
+++ src/game.js
@@ -1,73 +1,86 @@
-function Game(player) {}
+function Game(player1 = new Player(), player2 = new Player()) {}
 
 Game.prototype.result = function(player1, player2) {
-  if ((player1, player2)) {
-    this.matchDraw();
+  if (this.matchDraw(player1, player2)) {
+    return this.matchDraw(player1, player2);
+  } else if (this.rockWins(player1, player2)) {
+    return this.rockWins(player1, player2);
+  } else if (this.paperWins(player1, player2)) {
+    return this.paperWins(player1, player2);
+  } else if (this.scissorsWins(player1, player2)) {
+    return this.scissorsWins(player1, player2);
+  } else if (this.lizardWins(player1, player2)) {
+    return this.lizardWins(player1, player2);
+  } else if (this.spockWins(player1, player2)) {
+    return this.spockWins(player1, player2);
   }
 };
 
 Game.prototype.matchDraw = function(player1, player2) {
-  if (player1 === player2) {
+  if (player1.currentMove === player2.currentMove) {
     return "It's a draw!";
   }
 };
 
 Game.prototype.rockWins = function(player1, player2) {
-  if (player1 === "rock") {
+  if (player1.currentMove === "rock") {
   }
-  if (player2 === "scissors" || player2 === "lizard") {
-    return player1 + " wins!";
-  } else if (player2 === "rock") {
-    if (player1 === "scissors" || player1 === "lizard") {
-      return player2 + " wins!";
+  if (player2.currentMove === "scissors" || player2.currentMove === "lizard") {
+    return "Player1 wins!";
+  } else if (player2.currentMove === "rock") {
+    if (
+      player1.currentMove === "scissors" ||
+      player1.currentMove === "lizard"
+    ) {
+      return "Player2 wins!";
     }
   }
 };
 
 Game.prototype.paperWins = function(player1, player2) {
-  if (player1 === "paper") {
+  if (player1.currentMove === "paper") {
   }
-  if (player2 === "rock" || player2 === "Spock") {
-    return player1 + " wins!";
-  } else if (player2 === "paper") {
-    if (player1 === "rock" || player1 === "Spock") {
-      return player2 + " wins!";
+  if (player2.currentMove === "rock" || player2.currentMove === "Spock") {
+    return "Player1 wins!";
+  } else if (player2.currentMove === "paper") {
+    if (player1.currentMove === "rock" || player1.currentMove === "Spock") {
+      return "Player2 wins!";
     }
   }
 };
 
 Game.prototype.scissorsWins = function(player1, player2) {
-  if (player1 === "scissors") {
+  if (player1.currentMove === "scissors") {
   }
-  if (player2 === "paper" || player2 === "lizard") {
-    return player1 + " wins!";
-  } else if (player2 === "scissors") {
-    if (player1 === "paper" || player1 === "lizard") {
-      return player2 + " wins!";
+  if (player2.currentMove === "paper" || player2.currentMove === "lizard") {
+    return "Player1 wins!";
+  } else if (player2.currentMove === "scissors") {
+    if (player1.currentMove === "paper" || player1.currentMove === "lizard") {
+      return "Player2 wins!";
     }
   }
 };
 
 Game.prototype.lizardWins = function(player1, player2) {
-  if (player1 === "lizard") {
+  if (player1.currentMove === "lizard") {
   }
-  if (player2 === "paper" || player2 === "Spock") {
-    return player1 + " wins!";
-  } else if (player2 === "lizard") {
-    if (player1 === "paper" || player1 === "Spock") {
-      return player2 + " wins!";
+  if (player2.currentMove === "paper" || player2.currentMove === "Spock") {
+    return "Player1 wins!";
+  } else if (player2.currentMove === "lizard") {
+    if (player1.currentMove === "paper" || player1.currentMove === "Spock") {
+      return "Player2 wins!";
     }
   }
 };
 
 Game.prototype.spockWins = function(player1, player2) {
-  if (player1 === "Spock") {
+  if (player1.currentMove === "Spock") {
   }
-  if (player2 === "rock" || player2 === "scissors") {
-    return player1 + " wins!";
-  } else if (player2 === "Spock") {
-    if (player1 === "rock" || player1 === "scissors") {
-      return player2 + " wins!";
+  if (player2.currentMove === "rock" || player2.currentMove === "scissors") {
+    return "Player1 wins!";
+  } else if (player2.currentMove === "Spock") {
+    if (player1.currentMove === "rock" || player1.currentMove === "scissors") {
+      return "Player2 wins!";
     }
   }
 };
--- src/interface.js
+++ src/interface.js
@@ -1,30 +1,30 @@
-alert("Hey");
-
-$(document).ready(function() {
-  var player1 = new Player();
-  var player2 = new Player();
-  var game = new Game();
-
-  $("#btnMovePlayer1").click(function() {
-  event.preventDefault();
-  var movePlayer1 = $("#rpsls1").val();
-  displayMovePlayer1(movePlayer1);
-});
-
-    function displayMovePlayer1(movePlayer1) {
-      if (movePlayer1 === "rock") {
-        $("#player1").html("<i class='fa fa-hand-rock-o' aria-hidden="true"></i>");
-      } else if (movePlayer1 === "paper"){
-        $("#player1").html("<i class='fa fa-hand-paper-o' aria-hidden="true"></i>");
-      } else if (movePlayer1 === "scissors"){
-        $("#player1").html("<i class='fa fa-hand-scissors-o' aria-hidden="true"></i>");
-      } else if (movePlayer1 === "lizard"){
-        $("#player1").html("<i class='fa-hand-lizard-o' aria-hidden="true"></i>");
-      } else {
-        $("#player1").html("<i class='fa-hand-spock-o' aria-hidden="true"></i>");
-      };
-
-      };
-    };
-
-});
+// alert("Hey");
+//
+// $(document).ready(function() {
+//   var player1 = new Player();
+//   var player2 = new Player();
+//   var game = new Game();
+//
+//   $("#btnMovePlayer1").click(function() {
+//   event.preventDefault();
+//   var movePlayer1 = $("#rpsls1").val();
+//   displayMovePlayer1(movePlayer1);
+// });
+//
+//     function displayMovePlayer1(movePlayer1) {
+//       if (movePlayer1 === "rock") {
+//         $("#player1").html("<i class='fa fa-hand-rock-o' aria-hidden="true"></i>");
+//       } else if (movePlayer1 === "paper"){
+//         $("#player1").html("<i class='fa fa-hand-paper-o' aria-hidden="true"></i>");
+//       } else if (movePlayer1 === "scissors"){
+//         $("#player1").html("<i class='fa fa-hand-scissors-o' aria-hidden="true"></i>");
+//       } else if (movePlayer1 === "lizard"){
+//         $("#player1").html("<i class='fa-hand-lizard-o' aria-hidden="true"></i>");
+//       } else {
+//         $("#player1").html("<i class='fa-hand-spock-o' aria-hidden="true"></i>");
+//       };
+//
+//       };
+//     };
+//
+// });
