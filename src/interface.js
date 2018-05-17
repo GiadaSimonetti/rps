d5a977fb4e034fd245124db3427205a8a04e66d4
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-05-17 18:11:43 +0100

    User can play the game in the browser

--- src/interface.js
+++ src/interface.js
@@ -1,22 +1,14 @@
-// let player1 = new Player(move);
-// let player2 = new Player(move);
 let game = new Game();
 
-document.getElementById('submit1-btn').addEventListener('click', () => {
-  let move = document.getElementById('input-player1').value;
-  // let response = raindrops.plingPlangPlong(number);
-  document.getElementById('player1').innerHTML =
-    `<i class="fa fa-hand-` + move + `-o" aria-hidden="true"></i>`;
-});
+document.getElementById("submit-btn").addEventListener("click", () => {
+  let move1 = document.getElementById("input-player1").value;
+  let move2 = document.getElementById("input-player2").value;
+  let response = game.result(move1, move2);
 
-document.getElementById('submit2-btn').addEventListener('click', () => {
-  let move = document.getElementById('input-player2').value;
-  document.getElementById('player2').innerHTML =
-    `<i class="fa fa-hand-` + move + `-o" aria-hidden="true"></i>`;
-});
+  document.getElementById("player1").innerHTML =
+    `<i class="fa fa-hand-` + move1 + `-o" aria-hidden="true"></i> VS `;
+  document.getElementById("player2").innerHTML =
+    `<i class="fa fa-hand-` + move2 + `-o" aria-hidden="true"></i>`;
+  document.getElementById("match-result").innerHTML = response;
 
-// document.getElementById("rpssl1").addEventListener("change", () => {
-//   let playerOneMove = this.value;
-//   console.log(this.value);
-//   document.getElementById("player1").innerHTML = playerOneMove;
-// });
+});
