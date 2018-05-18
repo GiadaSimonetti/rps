844d0e4ca08711362aedc31f57f4eb7878d8d119
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-05-18 15:09:48 +0100

    User can choose their move and play against the computer

--- src/interface.js
+++ src/interface.js
@@ -1,15 +1,18 @@
 let game = new Game();
 let movesArray = ["rock", "paper", "scissors", "lizard", "spock"];
-let move1 = movesArray[Math.floor(Math.random()* movesArray.length)];
 let move2 = movesArray[Math.floor(Math.random()* movesArray.length)];
-let response = game.result(move1, move2);
 
-document.getElementById("submit-btn").addEventListener("click", () => {
+document.getElementById("submit-btn1").addEventListener("click", () => {
+  let move1 = document.getElementById("input-player1").value;
+  let response = game.result(move1, move2);
 
   document.getElementById("player1").innerHTML =
-    `<i class="fa fa-hand-` + move1 + `-o" aria-hidden="true"></i> VS `;
-  document.getElementById("player2").innerHTML =
-    `<i class="fa fa-hand-` + move2 + `-o" aria-hidden="true"></i>`;
-  document.getElementById("match-result").innerHTML = response;
+  `<i class="fa fa-hand-` + move1 + `-o" aria-hidden="true"></i>`;
+  document.getElementById("submit-btn2").addEventListener("click", () => {
 
+    document.getElementById("player2").innerHTML =
+    `VS <i class="fa fa-hand-` + move2 + `-o" aria-hidden="true"></i>`;
+    document.getElementById("match-result").innerHTML = response;
+
+  });
 });
