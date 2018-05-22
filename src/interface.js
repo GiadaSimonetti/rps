3c14eb4e9bb3b47e25c1297193258a1a1ce0c56d
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-05-22 15:46:26 +0100

    Style the game result + clean interface file

--- src/interface.js
+++ src/interface.js
@@ -5,14 +5,21 @@ let move2 = movesArray[Math.floor(Math.random()* movesArray.length)];
 document.getElementById("submit-btn1").addEventListener("click", () => {
   let move1 = document.getElementById("input-player1").value;
   let response = game.result(move1, move2);
+  let p1 = document.getElementById("player1");
+  let p2 = document.getElementById("player2");
+  let matchResult = document.getElementById("match-result");
 
-  document.getElementById("player1").innerHTML =
-  `<i class="fa fa-hand-` + move1 + `-o" aria-hidden="true"></i>`;
+  p1.innerHTML =
+  `<i class="fa fa-hand-` + move1 + `-o" aria-hidden="true"></i> `;
+  p1.style.fontSize= "100px";
+
   document.getElementById("submit-btn2").addEventListener("click", () => {
 
-    document.getElementById("player2").innerHTML =
+    p2.innerHTML =
     `VS <i class="fa fa-hand-` + move2 + `-o" aria-hidden="true"></i>`;
-    document.getElementById("match-result").innerHTML = response;
+    p2.style.fontSize= "100px";
+    matchResult.innerHTML = response;
+    matchResult.style.fontSize= "50px";
     document.getElementById("submit-btn2").style.visibility = "hidden";
     document.getElementById("submit-btn1").style.visibility = "hidden";
 
